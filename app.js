42c7c12f91f32330201bf3f3a0f4de475bbdd8b4
Sagar Maurya <147434812+asmigar@users.noreply.github.com>
2024-05-18 10:28:45 +0530

    add path to stop server

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const hostname = "127.0.0.1"
 const port = process.env.PORT
@@ -20,6 +21,24 @@ app.get('/org/:orgname', (req, res) => {
 
 app.get('/user/:username/org/:orgname', (req, res) => {
   res.send(`Hello ${req.params.username} with love from ${req.params.orgname}!`)
+})
+
+app.get('/bye', (req, res) => {
+  const options = {
+        root: path.join(__dirname)
+    };
+ 
+  const fileName = 'assets/bye.gif';
+    res.sendFile(fileName, options, function (err) {
+        if (err) {
+            console.error('Error sending file:', err);
+        } else {
+            console.log('Sent:', fileName);
+            process.exit(1)
+        }
+    });
+
+
 })
 
 app.listen(port, () => {
